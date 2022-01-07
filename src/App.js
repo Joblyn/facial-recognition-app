3f3af35fabd52f46f25aca6106b266b2f0120037
Joblyn <oaikhenahjob@gmail.com>
2022-01-07 14:56:11 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Navigation from './components/Navigation/navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-tsparticles'
 
-function App() {
+function App () {
+  const particlesInit = main => {
+    console.log(main)
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  }
+
+  const particlesLoaded = container => {
+    console.log(container)
+  }
+
+  const particleOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Particles
+        className='particles'
+        id='tsparticles'
+        url='http://foo.bar/particles.json'
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
