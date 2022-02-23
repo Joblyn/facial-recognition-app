7ef3f441a5035e8d198ae56849ca32c8c5c8e313
Joblyn <oaikhenahjob@gmail.com>
2022-02-23 13:27:08 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import './App.css'
-import Navigation from './components/Navigation/navigation'
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Rank from './components/Rank/Rank'
-import Particles from 'react-tsparticles'
+import "./App.css";
+import Navigation from "./components/Navigation/navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-tsparticles";
+import { useState } from "react";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+const app = Clarifai.app({
+  apiKey: "21860d09bea5458bbe1918a84500c1d4",
+});
 
-function App () {
-  const particlesInit = main => {
+function AppComponent() {
+  const [input, setInput] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState("signin");
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const particlesInit = (main) => {
     // console.log(main)
+  };
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  }
-
-  const particlesLoaded = container => {
-    console.log(container)
-  }
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
   const particleOptions = {
     particles: {
@@ -22,28 +34,79 @@ function App () {
         value: 30,
         density: {
           enable: true,
-          value_area: 800
-        }
-      }
+          value_area: 800,
+        },
+      },
+    },
+  };
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left.col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  };
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setImgUrl(input);
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, input)
+      .then((response) => displayFaceBox(calculateFaceLocation(response)))
+      .catch((err) => console.log(err));
+  };
+
+  const onRouteChange = (route) => {
+    if (route === "signout") {
+      setIsSignedIn(false); 
     }
-  }
+    else if (route === "home") {
+      setIsSignedIn(true);
+    }
+    setRoute(route);
+  };
   return (
-    <div className='App'>
+    <div className="App">
       <Particles
-        className='particles'
-        id='tsparticles'
-        url='http://foo.bar/particles.json'
+        className="particles"
+        id="tsparticles"
+        url="http://foo.bar/particles.json"
         init={particlesInit}
         loaded={particlesLoaded}
         options={particleOptions}
       />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === "home" ? (
+        <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition box={box} imgUrl={imgUrl} />
+        </div>
+      ) : route === "signin" ? (
+        <Signin onRouteChange={onRouteChange} />
+      ) : (
+        <Register onRouteChange={onRouteChange} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default AppComponent;
